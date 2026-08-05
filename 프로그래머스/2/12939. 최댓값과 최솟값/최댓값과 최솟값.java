47f404ab0ac524025459bab3ca36cbbd19e4ebbd
poul0102 <lday217617@gmail.com>
2026-08-05 21:51:07 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.23 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        StringTokenizer st = new StringTokenizer(s);
+        while (st.hasMoreTokens()) {
+            int cur = Integer.parseInt(st.nextToken());
+            max = Math.max(max, cur);
+            min = Math.min(min, cur);
+        }
+        
+        answer.append(min).append(" ").append(max);
+        
+        return answer.toString();
+    }
+}
