0d169736daf8f5d6612361dfc11219ae500fcc0d
poul0102 <lday217617@gmail.com>
2026-08-09 14:35:54 +0900

    [level 2] Title: 숫자의 표현, Time: 1.99 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int count = 1;
+        while (count <= n) {
+            int sum = 0;
+            for (int i = count; i <= n; i++) {
+                sum += i;
+                if (sum > n) break;
+                else if (sum == n) {
+                    answer++;
+                    break;
+                }
+            }
+            
+            count++;
+        }
+        
+        return answer;
+    }
+}
