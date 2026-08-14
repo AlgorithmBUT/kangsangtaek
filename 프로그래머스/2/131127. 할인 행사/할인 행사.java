c6e5d3af103bac747b00ebcbddaff264b9099f2b
poul0102 <lday217617@gmail.com>
2026-08-14 22:03:39 +0900

    [level 2] Title: 할인 행사, Time: 42.48 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
+            }
+            
+            boolean check = true;
+            for (int j = 0; j < want.length; j++) {
+                if (!map.containsKey(want[j]) || map.get(want[j]) != number[j]) {
+                    check = false;
+                    break;
+                }
+            }
+            
+            if (check) answer++;
+        }
+        
+        return answer;
+    }
+}
