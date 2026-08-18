01181c056cfc69251e6ce55366d3ed764bdf599e
poul0102 <lday217617@gmail.com>
2026-08-18 19:41:39 +0900

    [level 2] Title: 피로도, Time: 2.66 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    int answer = -1;
+    int[][] dungeons;
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        this.dungeons = dungeons;
+        visited = new boolean[dungeons.length];
+        
+        dfs(k, 0);
+        
+        return answer;
+    }
+    
+    void dfs(int k, int count) {
+        answer = Math.max(answer, count);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
