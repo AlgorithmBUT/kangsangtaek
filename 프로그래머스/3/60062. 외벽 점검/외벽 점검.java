d17bc55c1c2645d6a00f77f57752cf0be50414ab
poul0102 <lday217617@gmail.com>
2026-08-24 06:48:44 +0900

    [level 3] Title: 외벽 점검, Time: 37.35 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -0,0 +1,59 @@
+class Solution {
+    
+    int answer = Integer.MAX_VALUE;
+    int n;
+    int wLen;
+    int[] weak;
+    int[] dist;
+    boolean[] visited;
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        this.n = n;
+        
+        wLen = weak.length;
+        this.weak = new int[wLen * 2];
+        for (int i = 0; i < wLen; i++) {
+            this.weak[i] = weak[i];
+            this.weak[i + wLen] = weak[i] + n;
+        }
+        
+        this.dist = dist;
+        visited = new boolean[dist.length];
+        
+        for (int i = 0; i < wLen; i++) {
+            for (int j = 0; j < dist.length; j++) {
+                visited[j] = true;
+                dfs(i, j, 0, 1);
+                visited[j] = false;
+            }
+        }
+        
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+    
+    void dfs(int wIndex, int dIndex, int wCount, int fCount) {
+        if (fCount >= answer) return;
+        
+        int cur = weak[wIndex] + dist[dIndex];
+        int cnt = 0;
+        for (int i = wIndex; i < wIndex + wLen; i++) {
+            if (cur >= weak[i]) cnt++;
+            else break;
+        }
+        
+        if (wCount + cnt >= wLen) {
+            answer = Math.min(answer, fCount);
+            return;
+        }
+        
+        if (cnt == 0) return;
+        
+        for (int i = 0; i < dist.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(wIndex + cnt, i, wCount + cnt, fCount + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
